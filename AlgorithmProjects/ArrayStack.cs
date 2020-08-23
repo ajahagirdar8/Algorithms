6bb7b38f801018b307b9230fe8fe04e50d8e358a
Amruta Jahagirdar <ajahagirdar8@gmail.com>
2020-08-23 18:18:55 +0530

    initial

---
+++ AlgorithmProjects/ArrayStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace AlgorithmsProjects
+{
+    /***
+     * Generic Stack class using array as data structure
+     ***/
+    public class ArrayStack<T>
+    {
+        private T[] astack;
+        private int N;
+
+        public ArrayStack(int size)
+        {
+            astack = new T[size];
+        }
+
+        public Boolean isEmpty()
+        {
+            return N == 0;
+        }
+
+        public void push(T item)
+        {
+            astack[N++] = item;
+        }
+
+        public T pop()
+        {
+            return astack[--N];
+        }
+    }
+}
---
+++ AlgorithmProjects/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AlgorithmsProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success;
+            do
+            {
+                Console.WriteLine("Choose the data structure/algorithm to test:");
+                Console.WriteLine("1. Stack using array");
+                Console.WriteLine("2. Stack using linked list");
+                string input = Console.ReadLine();
+                int inputno;
+                success = Int32.TryParse(input, out inputno);
+                if (success)
+                {
+
+                    switch(inputno)
+                    {
+                        case 1:
+                            ArrayStackClient asc = new ArrayStackClient();
+                            asc.TestClinet();
+                            break;
+                        case 2:
+                            LinkedListStackClient a = new LinkedListStackClient();
+                            a.TestClient();
+                            break;
+                        default:
+                            Console.WriteLine("Enter a valid choice");
+                            success = false;
+                            break;
+
+                    }
+                    
+                }
+                else
+                    Console.WriteLine("Enter a valid choice");
+            } while (!success);
+
+
+           
+
+
+
+        }
+    }
+}
